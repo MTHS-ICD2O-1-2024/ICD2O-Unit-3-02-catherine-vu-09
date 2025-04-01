6c28a5888a26f861757c06bca2fae75973a2fc14
Catherine Vu <catherine.vu@stu.ocsb.ca>
2025-04-01 16:49:59 +0000

    added js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Catherine Vu All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Catherine Vu
+// Created on: April 1 2025
 // This file contains the JS functions for index.html
+
+/**
+* Enter length, width and height
+*/
+
+function calculatePyramidVolume() {
+            let a = parseFloat(document.getElementById('length').value);
+            let b = parseFloat(document.getElementById('width').value);
+            let h = parseFloat(document.getElementById('height').value);
+            if (!isNaN(a) && !isNaN(b) && !isNaN(h)) {
+               let area = ((l*w*h) / 3) * h;
+                document.getElementById('volume').innerText = `Volume is: ${area} mm²`;
+            } else {
+                document.getElementById('volume').innerText = 'Please enter valid numbers';
+            }
+        }
